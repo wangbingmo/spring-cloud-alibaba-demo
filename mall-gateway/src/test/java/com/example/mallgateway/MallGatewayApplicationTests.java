9cc79903cd6574f0240d7b867ef35f3e454cfd78
wangbingmo <hiwangbingmo@gmail.com>
2022-06-29 21:00:45 +0800

    add spring-cloud gateway

---
+++ mall-gateway/src/test/java/com/example/mallgateway/MallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mallgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
